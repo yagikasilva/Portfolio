4264bb0e4e42ee77929bfd0ec1742bc39662548e
YAGIKA_SILVA <116630871+yagikasilva@users.noreply.github.com>
2024-08-28 21:16:50 +0530

    script.js

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+
+var Emodal = document.getElementById("myEducation");
+
+// Get the button that opens the modal
+var Ebtn = document.getElementById("openEducationBtn");
+
+// Get the <span> element that closes the modal
+var Espan = document.getElementsByClassName("close1")[0];
+
+// When the user clicks the button, open the modal
+Ebtn.onclick = function() {
+    Emodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+Espan.onclick = function() {
+    Emodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == Emodal) {
+        Emodal.style.display = "none";
+    }
+}
